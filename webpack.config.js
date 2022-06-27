28a7ad94657bce9369c0e59df9f8820d50b7328d
Eternallyking <2499105321@qq.com>
2022-06-28 00:01:11 +0800

    完成了HTML的配置等功能

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
-const path=require('path')
-module.exports={
-    entry:'./src/index.js',
-    output:{
-        path:path.join(__dirname, 'lib'),
-        filename:'index.js',
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'lib'),
+        filename: 'index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            filename:'index.html',
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devServer:{
+        port: 3000,
+        open: true,
     }
 }
