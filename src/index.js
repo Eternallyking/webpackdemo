3902f4cbc7226edb674caf327bc37a8dc0aaa441
Eternallyking <2499105321@qq.com>
2022-06-28 00:55:13 +0800

    完成了打包图片的功能

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ import $ from 'jquery'
 $('#swiper').css('background', 'red')
 import './style/index.css'
 import './style/index.less'
+import imgUrl from './assets/1.gif'
+const img=document.createElement('img')
+img.src =imgUrl
+document.body.appendChild(img)
+import imgUrl1 from './assets/logo_small.png'
+const img1=document.createElement('img')
+img1.src =imgUrl1
+document.body.appendChild(img1)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-    mode:'development',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'lib'),
@@ -10,24 +10,44 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template:'./public/index.html',
-            filename:'index.html',
+            template: './public/index.html',
+            filename: 'index.html',
         }),
         new CleanWebpackPlugin()
     ],
-    devServer:{
+    devServer: {
         port: 3000,
         open: true,
     },
     module: {
-        rules:[
+        rules: [
             {
                 test: /\.css$/,
-                use: [ "style-loader", "css-loader"]
+                use: ["style-loader", "css-loader"]
             },
             {
                 test: /\.less$/,
-                use: [ "style-loader", "css-loader","less-loader"]
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            // {
+            //     test: /\.(png|jpg|gif|jpeg)$/,
+            //     use: [
+            //         {
+            //             loader: 'url-loader', 
+            //             options: {
+            //                 limit: 2 * 1024,
+            //             },
+            //         },
+            //     ],
+            // },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 2 * 1024
+                    }
+                }
             }
         ]
     },
